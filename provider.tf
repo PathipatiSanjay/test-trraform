8632d4cb5e2863f0ea6d90509f256798ad49e154
mounika <lingammounika2@gmail.com>
2025-05-09 10:19:37 +0530

    created a variables file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key =  "AKIAYHJANDB54VHMAPW6"
-  secret_key = "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"
+  access_key =  var.access_key
+  secret_key = var.secret_key
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "access_key" {
+    description = "Please enter your access key"  
+}
+
+variable "secret_key" {
+    description = "Please enter your secret key"  
+}
