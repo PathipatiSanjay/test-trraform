d0864c865d716d93cb90d743e6c5fdbb82bc928f
mounika <lingammounika2@gmail.com>
2025-05-09 10:47:11 +0530

    changed name

--- infra.tf
+++ infra.tf
@@ -1,5 +1,5 @@
   
-resource "aws_instance" "example" {
+resource "aws_instance" "test" {
     ami           = "ami-084568db4383264d4"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
